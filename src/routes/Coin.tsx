04895ab61c61ddda223fb0e2c4b507987204f8eb
WOOJIN SON <36991763+swj9707@users.noreply.github.com>
2022-08-10 22:52:20 +0900

    feature initSetup

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -196,7 +196,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
